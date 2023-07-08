0715617efb0767d943f20400ee0e7042607e1a16
Hashini <hashikavi1@gmail.com>
2023-07-08 11:52:02 +0530

    second commit

---
+++ models/posts.js
@@ -0,0 +1,21 @@
+const mongoose = require('module');
+
+const postSchema = new mongoose,Schema({
+
+    Topic:{
+        type:String,
+        required:true
+    },
+
+    description:{
+        type:String,
+        required : true
+    },
+
+    postCategory:{
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Posts', postSchema);
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 8000;
-const DB_URL = 'mongodb+srv://hashinik:Sandini123/@cluster0.zi2sjzo.mongodb.net/mernCrud?retryWrites=true&w=majority' //check whether the db name is necessary or not.
+const DB_URL = 'mongodb+srv://hashinik:Sandini123@cluster0.zi2sjzo.mongodb.net/retryWrites=true&w=majority' //check whether the db name is necessary or not.
 
-mongoose.connect(DB_URL)
+mongoose.connect(DB_URL) //didn't add the warnings because i didn't have any. time 17.10
+    .then(() =>{
+        console.log('DB connected');
+    })
+    .catch((err) => console.log('DB connection error',err))
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`)
