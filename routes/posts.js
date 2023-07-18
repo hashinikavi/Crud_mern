230aca2840b4c3d0e9dfbfe36d01a635aaacb046
Hashini <hashikavi1@gmail.com>
2023-07-19 00:11:16 +0530

    third commit

--- routes/posts.js
+++ routes/posts.js
@@ -27,9 +27,44 @@ router.post('/post/save', (req,res)=>{
 //get post
 
 router.get('./posts', (req,res) =>{
+    Posts.find().exec((err,posts) => {
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            existingPosts:posts
+        });
+    });
+});
+
+//update posts
+
+router.put('/post/update/:id', (req,res) =>{
+    Posts.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,post) =>{
+            if(err){
+                return res.status(400).json({error:err});
+            }
+
+            return res.status(200).json({
+                success : "updated successfully"
+            });
+        }
+    );
+});
+
+//delete post
+
+router.delete('/post/delete/:id',(req,res) =>{
     
 })
-
-
 
 module.exports = router;
