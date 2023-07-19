e191a7e7ad91d4aa7adc305fb3cdcd17ec0736f6
Hashini <hashikavi1@gmail.com>
2023-07-19 22:07:37 +0530

    updated

--- routes/posts.js
+++ routes/posts.js
@@ -26,19 +26,19 @@ router.post('/post/save', (req,res)=>{
 
 //get post
 
-router.get('./posts', (req,res) =>{
-    Posts.find().exec((err,posts) => {
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-
+router.get('/posts', (req,res) =>{
+    Posts.find()
+    .then((posts) =>{
         return res.status(200).json({
             success:true,
             existingPosts:posts
         });
-    });
+    })
+    .catch(err => {
+        return res.status(400).json({
+            error:err
+        });
+    })
 });
 
 //update posts
@@ -48,23 +48,30 @@ router.put('/post/update/:id', (req,res) =>{
         req.params.id,
         {
             $set:req.body
-        },
-        (err,post) =>{
-            if(err){
-                return res.status(400).json({error:err});
-            }
-
+        })
+        .then(post =>{
             return res.status(200).json({
                 success : "updated successfully"
             });
-        }
-    );
+        })
+
+        .catch(err =>{
+            return res.status(400).json({error:err});
+        });
 });
 
 //delete post
 
 router.delete('/post/delete/:id',(req,res) =>{
-    
+    Posts.findByIdAndRemove(req.params.id).exec((err,deletePost) =>{
+        if(err) return res.status(400).json({
+            message:"Delete unsuccesful",err
+        });
+
+        return re.json({
+            message:"Delete successful", deletePost
+        })
+    })
 })
 
 module.exports = router;
